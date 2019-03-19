e76fbd54989f857aca99491aab396aee6ec902f3
Dhara Patel <1991dharapatel@gmail.com>
2019-03-19 13:35:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,6 @@ def batch_badge_creator(attendees)
   return "array of badge messages."
 end
 
-def assign_rooms(attendees)
-  return "array of badge messages."
-end
+# def assign_rooms(attendees)
+  # return ""Hello, #{attendees}! You'll be assigned to room _____!""
+# end
