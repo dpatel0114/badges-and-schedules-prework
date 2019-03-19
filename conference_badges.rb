43a29163d3d2439101c285fc5f80e4617ffbc47c
Dhara Patel <1991dharapatel@gmail.com>
2019-03-19 14:16:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
-puts  assign_rooms(attendees)
+  puts batch_badge_creator(attendees)
+ assign_rooms(attendees)
 
 end
